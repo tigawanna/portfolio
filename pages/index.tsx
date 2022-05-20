c12b6f96f542f727adfd1eb7a74375f98605e8ab
tigawanna <denniskinuthiaw@gmail.com>
2022-05-20 17:43:26 +0300

    added proect manager repo

--- pages/index.tsx
+++ pages/index.tsx
@@ -51,6 +51,12 @@ const Home: NextPage = () => {
           <Link href="https://linkedin.com/in/dennis-kinuthia"
           ><a target="_blank" className="text-green-400">dennis kinuthia</a></Link>
         </div>
+
+        <div className='my-2 md:my-0 text-sm md:text-lg text-slate-300 font-mono'>
+          Articles:
+          <Link href="https://dev.to/tigawanna"
+          ><a target="_blank" className="text-green-400">tigawanna</a></Link>
+        </div>
       </footer>
     </div>
   )
--- util/aboututils.ts
+++ util/aboututils.ts
@@ -46,19 +46,26 @@ export const backend=[
 
 
 export const projects =[
+
     {
-    title:"Chat App",
-    desc:"Built Using React for the front end and Nodejsfor the bac end with mongodb and graphql websockets ",
-    link:"https://github.com/tigawanna/chat"
-    },
-    {
-        title:"Property",
-        desc:" React for the front end and firebase to shop details and map every relvenat input to the resppective CRUD used my custom react table component field in a firebase backend ",
-        link:"https://github.com/tigawanna/property"
+        title:"Project manager",
+        desc:"react app to help coodination , employees add project proposala ,the manager approves or rejects them , then they're moved to the funding phase then the eployees execute amd makr them done when completed",
+        link:"https://dev.to/tigawanna/another-react-firebase-tutorial-34d0"
     },
     {
         title:"Edit Table",
         desc:"plain html table component for react which allows you to create ,read, update, and delete directly from the table, i made this after havng a hard time finding a table library to use in the propert manager app that meets the crud capabilities and gives me enough control",
         link:"https://github.com/tigawanna/edit-table"
-    }
+    },
+    {
+        title:"Chat App",
+        desc:"Built Using React for the front end and Nodejsfor the bac end with mongodb and graphql websockets ",
+        link:"https://github.com/tigawanna/chat"
+        },
+        
+    {
+        title:"Property",
+        desc:" React for the front end and firebase to shop details and map every relvenat input to the resppective CRUD used my custom react table component field in a firebase backend ",
+        link:"https://github.com/tigawanna/property"
+    },
 ]
